df78ef0be88e2ed6a1c812559ca52d978e80eb55
Senchenko Nikita <63493300+Senchenko-Dev@users.noreply.github.com>
2024-12-09 10:51:56 +0300

    Create start_all_service.sh

---
+++ start_all_service.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+folders=("am"
+         "verification"
+         "discovery"
+         "operhub"
+         "awh"
+         "haproxy"
+         "esbv3conversion"
+         "opmessagestosendsms"
+         "savecustomerips"
+         "atmtransactionverificationservicedocker"
+         "sbpaymiddleware"
+         "apcommon"
+         "saf"
+         "esb2amconversion"
+         "aprecurrent"
+)
+
+for folder in "${folders[@]}"; do
+    current_dir=$(pwd)
+    cd "$folder" || continue
+    for script in *.sh; do
+      if [ -f "$script" ]; then
+        echo "Запуск скрипта: $script в директории: $folder"
+        bash "$script"
+      fi
+    done
+
+    cd "$current_dir" || exit
+done
