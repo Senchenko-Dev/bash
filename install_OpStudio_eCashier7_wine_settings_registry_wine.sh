10c93538a8031b73193ffea89c59840f1794a6da
Senchenko Nikita <63493300+Senchenko-Dev@users.noreply.github.com>
2025-03-05 14:36:35 +0300

    Update install_OpStudio_eCashier7_wine_settings_registry_wine.sh

--- install_OpStudio_eCashier7_wine_settings_registry_wine.sh
+++ install_OpStudio_eCashier7_wine_settings_registry_wine.sh
@@ -2,64 +2,119 @@
 
 DIR="$(dirname "$0")"
 
-cd ${DIR} || exit
+cd "${DIR}" || exit
 
 base_dir="$HOME/.cache/winetricks"
-
 ekassir_dir="/opt/eKassir"
-
 file_path="/usr/bin/wine"
+opstudio="$HOME/Desktop/OpStudio.desktop"
+ecashier="$HOME/Desktop/eCashier.desktop"
+new_ekassir_dir="$HOME/eKassir"
 
-user_home="$HOME/Desktop/OpStudio.desktop"
-
-if [[ -f "${file_path}" ]]; then
-        echo "wine установлен"
-else
-    sudo apt install -y wine
+if [ -z "$1" ]; then
+  echo "Использование: $0 имя_пользователя"
+  exit 1
 fi
 
-if [[ -d "${ekassir_dir}" ]]; then
-    echo "eCashier7 и OperationStudio установлены"
-else
-   for deb in *.deb
-   do
-     sudo dpkg -i "${deb}"
-   done
+USERNAME=$1
 
-   printf "файлы находятся по пути /opt/eKassir:\n$(ls -la /opt/eKassir)\n"
+sudo astra-sudo-control disable
+
+sudo -u "${USERNAME}" bash -c "
+if [[ -f '${file_path}' ]]; then
+    echo 'wine установлен';
+else
+    sudo apt install -y wine;
 fi
+"
 
+sudo -u "${USERNAME}" bash -c "
+    echo 'Устанавливаем eCashier7 и OperationStudio'
+    for deb in *.deb; do
+        sudo dpkg -i \"\${deb}\";
+    done;
+"
 
-if [[ -f "${user_home}" ]]; then
-  echo "Ярлык создан"
+sudo -u "${USERNAME}" bash -c "
+if [[ -d '${new_ekassir_dir}' ]]; then
+   echo 'папка перемещена и установлены парва пользователя'
 else
-  echo '[Desktop Entry]
-  Name=OpStudio.desktop
-  Exec=wine '/opt/eKassir/PaySystemStudio/studio.exe'
-  Type=Application
-  Path=opt/eKassir/PaySystemStudio/' | sudo tee "${user_home}"
+     echo 'перносим папку eKassir в ${HOME} пользователя ${USERNAME}'
+     sudo chown -R "${USERNAME}:${USERNAME}" /opt/eKassir
+     sudo mv /opt/eKassir "$HOME"
 fi
+"
 
-
-if grep -q "X11 Driver" ~/.wine/user.reg; then
-    echo "Реестр настроен"
+sudo -u "${USERNAME}" bash -c "
+if [[ -f '${opstudio}' ]]; then
+    echo 'Ярлык OperationStudio создан';
 else
+    echo 'Создаем ярлык OperationStudio'
+    echo '[Desktop Entry]
+Name=OpStudio
+Exec=wine \"${HOME}/eKassir/PaySystemStudio/studio.exe\"
+Type=Application
+Path=${HOME}/eKassir/PaySystemStudio/' | sudo tee '${opstudio}' > /dev/null;
+fi
+"
+
+sudo -u "${USERNAME}" bash -c "
+if [[ -f '${ecashier}' ]]; then
+    echo 'удалил старый ярлык eCashier7';
+    rm -rf '${ecashier}';
+fi
+"
+
+sudo -u "${USERNAME}" bash -c "
+if [[ ! -f '${ecashier}' ]]; then
+    echo 'создаем новый ярлык eCashier7';
+    echo '[Desktop Entry]
+Name=eCashier7
+Exec=wine \"${HOME}/eKassir/eCashier7/Starter.exe\"
+Type=Application
+Path=${HOME}/eKassir/eCashier7/
+Icon=${HOME}/eKassir/eCashier7/logo/eCashier.ico' | sudo tee '${ecashier}' > /dev/null;
+fi
+"
+
+sudo -u "${USERNAME}" bash -c "
+if grep -q 'X11 Driver' ~/.wine/user.reg; then
+    echo 'Реестр настроен';
+else
+   echo 'Настраиваем реестр wine'
    echo 'Windows Registry Editor Version 5.00
 
-   [HKEY_CURRENT_USER\Software\Wine\X11 Driver]
-   "UseXVidMode"="N"' | sudo tee wine_settings.reg > /dev/null
+   [HKEY_CURRENT_USER\\Software\\Wine\\X11 Driver]
+   \"UseXVidMode\"=\"N\"' | sudo tee wine_settings.reg > /dev/null;
 
-   wine regedit wine_settings.reg
+   wine regedit wine_settings.reg;
 fi
+"
 
-
-if [[ -d "$base_dir" && -d "$base_dir/corefonts" && -d "$base_dir/tahoma" ]]; then
-
-        echo "Шрифты установлены"
+sudo -u "${USERNAME}" bash -c "
+if [[ -d '${base_dir}' && -d '${base_dir}/corefonts' && -d '${base_dir}/tahoma' ]]; then
+        echo 'Шрифты установлены';
 else
-        sudo unzip fonts.zip && cd fonts
-        sudo mkdir -p ~/.cache/winetricks/corefonts && sudo mkdir -p ~/.cache/winetricks/tahoma
-        sudo mv arial32.exe arialb32.exe ~/.cache/winetricks/corefonts
-        sudo mv IELPKTH.zip  ~/.cache/winetricks/tahoma
-        cd  ~/.cache/winetricks/tahoma && sudo unzip IELPKTH.zip && sudo mv IELPKTH/* . && sudo rm -rf IELPKTH.zip IELPKTH
+        echo 'Устанавливаем Шрифты'
+        sudo unzip fonts.zip && cd fonts;
+        sudo mkdir -p ~/.cache/winetricks/corefonts && sudo mkdir -p ~/.cache/winetricks/tahoma;
+        sudo mv arial32.exe arialb32.exe ~/.cache/winetricks/corefonts;
+        sudo mv IELPKTH.zip ~/.cache/winetricks/tahoma;
+        cd ~/.cache/winetricks/tahoma && sudo unzip IELPKTH.zip && sudo mv IELPKTH/* . && sudo rm -rf IELPKTH.zip IELPKTH;
 fi
+"
+
+if [[ -d  "${HOME}/Desktop/auto_install" ]]; then
+     echo "удаляем папку auto_install и zip архив"
+     sudo rm -rf "${HOME}/Desktop/auto_install"
+     sudo rm -rf "${HOME}/Desktop/auto_install.zip"
+fi
+
+if sudo grep -q "^${USERNAME} " /etc/sudoers; then
+    echo "Забираем root парава у пользователя ${USERNAME}"
+    sudo sed -i '/^'"${USERNAME}"' ALL=(ALL:ALL) ALL/d' /etc/sudoers
+fi
+
+#sudo astra-sudo-control enable
+
+#sudo deluser "${USERNAME}" sudo
